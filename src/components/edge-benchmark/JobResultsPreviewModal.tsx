2707abd439c40880e23a4af3b6f9af469a5fc1b0
Andreas Schliebitz <a.schliebitz@hs-osnabrueck.de>
2025-05-04 00:13:50 +0200

    Adding chart.js based visualization of benchmark job resource utilization.

--- src/components/edge-benchmark/JobResultsPreviewModal.tsx
+++ src/components/edge-benchmark/JobResultsPreviewModal.tsx
@@ -9,6 +9,8 @@
 //
 // SPDX-License-Identifier: MIT
 
+import 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
 import Grid from '@mui/material/Grid';
 import Dialog from '@mui/material/Dialog';
 import Button from '@mui/material/Button';
@@ -27,16 +29,217 @@ export default function ({
     benchmarkJobResult: Record<string, any>;
     onClose: () => void;
 }) {
+    const isNumber = (value: any) => {
+        return typeof value === 'number' && !isNaN(value);
+    };
+
+    const postprocess = (series: any[]): any[] => {
+        const mean = meanX(series, true);
+        series = series.map((value: any) => {
+            if (isNumber(value)) if (value < 0) return mean;
+            return value;
+        });
+        return series;
+    };
+
+    const getBenchmarkResults = () => {
+        return benchmarkJobResult.benchmark_job.benchmark_results;
+    };
+
+    const getSeries = (name: string): any[] => {
+        return postprocess(getBenchmarkResults()[name]);
+    };
+
+    const meanX = (series: number[], ignoreNegative: boolean): number => {
+        let sum_x = 0,
+            counter = 0;
+        for (let x = 0; x < series.length; ++x) {
+            if (ignoreNegative && series[x] < 0) continue;
+            sum_x += series[x];
+            counter++;
+        }
+        if (!counter) return 0;
+        return sum_x / counter;
+    };
+
+    const meanY = (series: number[][]): number[] => {
+        const means: number[] = [];
+        for (let x = 0; x < series[0].length; ++x) {
+            let sum_y = 0;
+            for (let y = 0; y < series.length; ++y) sum_y += series[y][x];
+            const mean_y = sum_y / series.length;
+            means.push(mean_y);
+        }
+        return means;
+    };
+
     return (
         <>
-            <Dialog open onClose={onClose} fullWidth maxWidth="sm">
+            <Dialog open onClose={onClose} fullWidth maxWidth="xl">
                 <DialogTitle>
-                    Results preview for {benchmarkJob.model.name} with {benchmarkJob.dataset.name} on{' '}
-                    {benchmarkJob.edge_device}
+                    Benchmark results for model {benchmarkJob.model.name} with dataset {benchmarkJob.dataset.name} on
+                    device {benchmarkJob.edge_device}
                 </DialogTitle>
                 <DialogContent>
                     <Grid container justifyContent="space-between">
-                        <Grid item xs={12}>
+                        <Grid item xs={8}>
+                            <Line
+                                data={{
+                                    labels: getSeries('time').map((datetime: string) => {
+                                        const date = new Date(datetime);
+                                        return date.toLocaleTimeString('en-US', {
+                                            hour12: false,
+                                            hour: '2-digit',
+                                            minute: '2-digit',
+                                            second: '2-digit',
+                                        });
+                                    }),
+                                    datasets: [
+                                        ...[
+                                            {
+                                                label: 'CPU',
+                                                data: meanY(
+                                                    Object.keys(getBenchmarkResults())
+                                                        .filter((key) => key.startsWith('CPU'))
+                                                        .map(getSeries),
+                                                ),
+                                            },
+                                            { label: 'GPU', data: getSeries('GPU') },
+                                            {
+                                                label: 'RAM',
+                                                data: getSeries('RAM').map((value) => value * 100),
+                                            },
+                                            {
+                                                label: 'SWAP',
+                                                data: getSeries('SWAP').map((value) => value * 100),
+                                            },
+                                            {
+                                                label: 'EMC',
+                                                data: getSeries('EMC').map((value) => value * 100),
+                                            },
+                                        ],
+                                        ...Object.keys(getBenchmarkResults())
+                                            .filter((key) => key.startsWith('Fan'))
+                                            .map((key) => ({
+                                                label: key,
+                                                data: getSeries(key),
+                                            })),
+                                    ].filter((dataset) =>
+                                        dataset.data.reduce((acc: number, curr: number) => acc + curr, 0),
+                                    ),
+                                }}
+                                options={{
+                                    responsive: true,
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            text: 'Utilization',
+                                        },
+                                    },
+                                    scales: {
+                                        x: {
+                                            title: {
+                                                display: true,
+                                                text: 'Time',
+                                            },
+                                        },
+                                        y: {
+                                            title: {
+                                                display: true,
+                                                text: 'Load (%)',
+                                            },
+                                        },
+                                    },
+                                }}
+                            />
+                            <Line
+                                data={{
+                                    labels: getSeries('time').map((datetime: string) => {
+                                        const date = new Date(datetime);
+                                        return date.toLocaleTimeString('en-US', {
+                                            hour12: false,
+                                            hour: '2-digit',
+                                            minute: '2-digit',
+                                            second: '2-digit',
+                                        });
+                                    }),
+                                    datasets: Object.keys(getBenchmarkResults())
+                                        .filter((key) => key.startsWith('Temp'))
+                                        .map((key) => ({
+                                            label: key,
+                                            data: getSeries(key),
+                                        }))
+                                        .filter((dataset) => dataset.data.reduce((acc, curr) => acc + curr, 0)),
+                                }}
+                                options={{
+                                    responsive: true,
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            text: 'Temperatures',
+                                        },
+                                    },
+                                    scales: {
+                                        x: {
+                                            title: {
+                                                display: true,
+                                                text: 'Time',
+                                            },
+                                        },
+                                        y: {
+                                            title: {
+                                                display: true,
+                                                text: 'Temperature (°C)',
+                                            },
+                                        },
+                                    },
+                                }}
+                            />
+                            <Line
+                                data={{
+                                    labels: getSeries('time').map((datetime: string) => {
+                                        const date = new Date(datetime);
+                                        return date.toLocaleTimeString('en-US', {
+                                            hour12: false,
+                                            hour: '2-digit',
+                                            minute: '2-digit',
+                                            second: '2-digit',
+                                        });
+                                    }),
+                                    datasets: Object.keys(getBenchmarkResults())
+                                        .filter((key) => key.startsWith('Power'))
+                                        .map((key) => ({
+                                            label: key,
+                                            data: getSeries(key).map((value) => value / 1000),
+                                        }))
+                                        .filter((dataset) => dataset.data.reduce((acc, curr) => acc + curr, 0)),
+                                }}
+                                options={{
+                                    responsive: true,
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            text: 'Power consumption',
+                                        },
+                                    },
+                                    scales: {
+                                        x: {
+                                            title: {
+                                                display: true,
+                                                text: 'Time',
+                                            },
+                                        },
+                                        y: {
+                                            title: {
+                                                display: true,
+                                                text: 'Power (W)',
+                                            },
+                                        },
+                                    },
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={4}>
                             {benchmarkJobResult && (
                                 <CodeEditor
                                     value={JSON.stringify(benchmarkJobResult, null, 4)}
