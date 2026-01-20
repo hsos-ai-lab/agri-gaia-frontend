c969dfcd74bb972fc82cabafa62f8b3e39ac5da6
Andreas Schliebitz <a.schliebitz@hs-osnabrueck.de>
2026-01-20 08:46:31 +0100

    Added overview of performance ids

--- src/components/edge-benchmark/JobResultsPreviewModal.tsx
+++ src/components/edge-benchmark/JobResultsPreviewModal.tsx
@@ -14,6 +14,14 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import CodeEditor from '@uiw/react-textarea-code-editor';
 import IBenchmarkJob from '../../types/edge-benchmark/IBenchmarkJob';
+import Paper from '@mui/material/Paper';
+import TableContainer from '@mui/material/TableContainer';
+import TableCell from '@mui/material/TableCell';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
 
 export default function ({
     benchmarkJob,
@@ -71,7 +79,32 @@ export default function ({
         }
         return means;
     };
+    const benchmark_performance = benchmarkJobResult.benchmark_job.inference_results.performance
+    const floating_precision = 4
+    
+    function getByPath(obj: any, path: string) { 
+        return path.split('.').reduce((acc, key) => acc?.[key], obj);
+    }
 
+    const processingStep = [
+        "preprocess",
+        "inference",
+        "postprocess",
+    ]
+    
+    function createRowWithNameAndValues(name: string, value: string): string[] { 
+        const list: string[] = [name]
+        processingStep.map(step => {
+            list.push(getByPath(benchmark_performance, step + value).toFixed(floating_precision).toString());
+        })
+        return list
+    }
+
+    const rows = [
+        createRowWithNameAndValues("Total Time", ".total_time"),
+        createRowWithNameAndValues("Samples Per Second", ".samples_per_second"),
+        createRowWithNameAndValues("Average Latency", ".latency.average"),
+    ];
     return (
         <>
             <Dialog open onClose={onClose} fullWidth maxWidth="xl">
@@ -80,8 +113,42 @@ export default function ({
                     " on device "{benchmarkJob.edge_device}"
                 </DialogTitle>
                 <DialogContent>
-                    <Grid container justifyContent="space-between">
+                    <Grid container justifyContent="space-around">
                         <Grid item xs={8}>
+                                <Typography 
+                                sx={{ mb: 2 }}
+                                gutterBottom>
+                                    Key Performance Indicators on {benchmark_performance.preprocess.sample_count.toFixed(0)} Samples
+                                </Typography>
+                            <TableContainer component={Paper}>
+                                <Table aria-label="hot overview">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Value</TableCell>
+                                            {processingStep.map(function(object, i){
+                                                return <TableCell align="right">{String(object).charAt(0).toUpperCase() + String(object).slice(1)}</TableCell>;
+                                            })}
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                    {rows.map((row) => (
+                                        <TableRow
+                                        key={row[0]}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                        <TableCell component="th" scope="row">
+                                            {row[0]}
+                                        </TableCell>
+                                        {row.map(function(object, i){
+                                            if(i >= 1) {
+                                                return <TableCell align="right">{object}</TableCell>;
+                                            }
+                                        })}
+                                        </TableRow>
+                                    ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
                             <Line
                                 data={{
                                     labels: getSeries('time').map((datetime: string) => {
